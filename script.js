b8875e1dcf8f9f263aef214632c6ea4dc3e4dc2b
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-07-30 21:21:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // your code here
+document.getElementById('button').addEventListener('click', function() {
+    var name = document.getElementById('name').value;
+    var year = document.getElementById('year').value;
+    
+    var newUrl = 'https://localhost:8080/?name=' + name + '&year=' + year;
+    
+    document.getElementById('url').innerText = newUrl;
+});
