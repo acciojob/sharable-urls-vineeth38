db4fa01936a4907f263ea99571ddbc7fce7d401f
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-07-30 21:43:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-// your code here
 document.getElementById('button').addEventListener('click', function() {
     var name = document.getElementById('name').value;
-    var year = document.getElementById('year').value;
-    
-    var params = [];
-    if(name) {
-        params.push('name=' + encodeURIComponent(name));
-    }
-    if(year) {
-        params.push('year=' + encodeURIComponent(year));
-    }
-    
-    params.sort();
+    var year = parseInt(document.getElementById('year').value);
     
     var newUrl = 'https://localhost:8080/';
-    if(params.length > 0) {
-        newUrl += '?' + params.join('&');
+    if(name) {
+        newUrl += '?name=' + encodeURIComponent(name);
+        if(!isNaN(year)) {
+            newUrl += '&year=' + year;
+        }
+    } else if(!isNaN(year)) {
+        newUrl += '?year=' + year;
     }
     
     document.getElementById('url').innerText = newUrl;
 });
+
 
     
    
