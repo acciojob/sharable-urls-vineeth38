aaa7438a80a46ff3872b6ea5a6a159501819f18a
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-07-30 21:30:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,15 @@ document.getElementById('button').addEventListener('click', function() {
     var name = document.getElementById('name').value;
     var year = document.getElementById('year').value;
     
-    var newUrl = 'https://localhost:8080/?name=' + name + '&year=' + year;
+    var newUrl = 'https://localhost:8080/';
+    if(name) {
+        newUrl += '?name=' + encodeURIComponent(name);
+        if(year) {
+            newUrl += '&year=' + encodeURIComponent(year);
+        }
+    } else if(year) {
+        newUrl += '?year=' + encodeURIComponent(year);
+    }
     
     document.getElementById('url').innerText = newUrl;
 });
